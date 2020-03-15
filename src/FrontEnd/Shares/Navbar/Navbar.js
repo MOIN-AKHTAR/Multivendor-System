e288f6f42b87249c5a91e3399df38386bdf882f9
MOIN-AKHTAR <moinakhter179@gmail.com>
2020-03-15 18:17:38 +0500

    Completed Navbar And SideNavBar When Screen Size Is Small SideNavBar Appear And When Screen Size Is Large Then Main NavBar Will Be Shown

--- src/FrontEnd/Shares/Navbar/Navbar.js
+++ src/FrontEnd/Shares/Navbar/Navbar.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import SideNavBar from "../SideNavBar/SideNavBar";
+import Background from "../Background/Background";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [showSideNavBar, setShowSideNavBar] = useState(false);
+  const ShowSideNavBar = useCallback(() => {
+    setShowSideNavBar(prev => !prev);
+  }, []);
   return (
-    <div id="navbar">
-      <div id="Logo">Multivendor System</div>
-      <ul>
-        <li>
-          <Link to="/" className="Links">
-            All Items
-          </Link>
-        </li>
-        <li>
-          <Link to="/auth" className="Links">
-            Authenticate
-          </Link>
-        </li>
-      </ul>
+    <div>
+      {showSideNavBar && (
+        <>
+          <Background />
+          <SideNavBar ShowSideNavBar={ShowSideNavBar} />
+        </>
+      )}
+      <div id="navbar">
+        <div id="Logo_Btn__Section">
+          <div id="Side_Btn" onClick={ShowSideNavBar}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div id="Logo">Multivendor System</div>
+        </div>
+        <ul>
+          <li>
+            <Link to="/" className="Links">
+              All Items
+            </Link>
+          </li>
+          <li>
+            <Link to="/auth" className="Links">
+              Authenticate
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/FrontEnd/Shares/SideNavBar/SideNavBar.js
+++ src/FrontEnd/Shares/SideNavBar/SideNavBar.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./SideNavBar.css";
 
-function SideNavBar() {
+function SideNavBar(props) {
   return (
-    <div id="Side_Nav_Bar">
+    // Here ShowSideNavBar Is A FunctionWhihc Will Be Called When Yiu Click On SideNavBar's List Item-
+    <div id="Side_Nav_Bar" onClick={props.ShowSideNavBar}>
       <li>
-        <Link to="/" className="Links">
+        <Link to="/" className="Side_Links">
           All Items
         </Link>
       </li>
       <li>
-        <Link to="/auth" className="Links">
+        <Link to="/auth" className="Side_Links">
           Authenticate
         </Link>
       </li>
